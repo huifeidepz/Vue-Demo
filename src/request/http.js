f006bbee00969e00106821557d8ad3872327f6f9
余辉武 <2331586177@qq.com>
2021-04-21 17:23:04 +0800

    修复了请求失败的无法获取数据的问题

--- src/request/http.js
+++ src/request/http.js
@@ -1,11 +1,37 @@
 import axios from 'axios';
 
-const baseUrl = "http://127.0.0.1:5000/"
+//const baseUrl = "http://127.0.0.1:5000/"
 // const baseUrl = "/api/"
-// const baseUrl = "http://47.98.108.37/api/"
+const baseUrl = "http://47.98.108.37/api/"
 axios.defaults.timeout = 10000;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
-
+//在main.js设置全局的请求次数，请求的间隙
+axios.defaults.retry = 4;
+axios.defaults.retryDelay = 1000;
+axios.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
+    var config = err.config;
+    // If config does not exist or the retry option is not set, reject
+    if(!config || !config.retry) return Promise.reject(err);
+    // Set the variable for keeping track of the retry count
+    config.__retryCount = config.__retryCount || 0;
+    // Check if we've maxed out the total number of retries
+    if(config.__retryCount >= config.retry) {
+        // Reject with the error
+        return Promise.reject(err);
+    }
+    // Increase the retry count
+    config.__retryCount += 1;
+    // Create new promise to handle exponential backoff
+    var backoff = new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve();
+        }, config.retryDelay || 1);
+    });
+    // Return the promise in which recalls axios to retry the request
+    return backoff.then(function() {
+        return axios(config);
+    });
+});
 export function get(url, params) {
   return axios.get( baseUrl+url, {
     params: params
